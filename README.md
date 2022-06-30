05d70f1ff9782a5837a6d4b03756095b45f8bc33
yinshawnrao <yinshawnrao@tencent.com>
2022-06-30 12:17:22 +0800

    提交cos插件

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# ghost-cos-store
+# Ghost COS Store
+This Ghost custom storage module allows you to store media file with TencentCloud COS instead of storing at local machine.
+
+## Installation
+
+### Via NPM
+
+- Install COS storage module
+
+  ```
+  npm install ghost-cos-store
+  ```
+  
+- Make the storage folder if it doesn't exist yet
+
+  ```
+  mkdir -p content/adapters/storage
+  ```
+  
+ - Create a script named "cos-store.js", content as follow:
+ 
+ ```javascript
+//  content/adapters/storage/cos-store.js
+module.exports = require('ghost-cos-store');
+ ```
+
+### Via Git
+In order to replace the storage module, the basic requirements are:
+
+- Create a new folder named `storage` inside `content/adapters`
+- Clone this repo to `/storage`
+```
+mkdir -p [ghost/ptah]/content/adapters/storage
+cd [ghost/ptah]/content/adapters/storage
+
+git clone https://github.com/tencentyun/ghost-cos-store.git
+```
+- Install dependencies
+```
+cd ghost-cos-store
+npm i
+```
+
+## Configuration
+
+In your `config.[env].json` file, you'll need to add a new storage block to whichever environment you want to change:
+
+```json
+{
+  "storage": {
+    "active": "cos-store",
+    "cos-store": {      
+      "BasePath": "资源路径，不填写默认为根目录",      
+      "SecretId": "SecretId，在腾讯云控制台获取",
+      "SecretKey": "SecretKey，在腾讯云控制台获取",
+      "Bucket": "<BucketName>-<AppId>，如 test-1250000000",
+      "Region": "<Region>，如 ap-chengdu"
+    }
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,106 @@
+const COS = require('cos-nodejs-sdk-v5')
+const BaseStore = require('ghost-storage-base')
+const path = require('path')
+
+class CosStore extends BaseStore {
+  
+  constructor (config) {
+    super(config)
+
+    this.baseParams = {
+      Bucket: config.Bucket,
+      Region: config.Region
+    }
+
+    this.basePath = config.BasePath || '';
+
+    this.client = new COS({
+      SecretId: config.SecretId,
+      SecretKey: config.SecretKey
+    })
+    
+  }
+
+  exists (filename) {
+    return new Promise((resolve, reject) => {
+      this.client.headObject({
+        ...baseParams,
+        Key: this.basePath + filename
+      }, (err, data) => {
+        if(err) {
+          if(err.code == 404) {
+            resolve(false)
+          }else {
+            reject(this.errorParser(err))  
+          }          
+        }else {
+          resolve(true)
+        }
+      })
+    })
+  }
+
+  save (filename) {
+    return new Promise((resolve, reject) => {
+      this.client.putObject({
+        ...baseParams,
+        Key: this.basePath + filename
+      }, (err, data) => {
+        if(err) {
+          reject(this.errorParser(err))   
+        }else {
+          resolve(data.Location)
+        }
+      })
+    })
+  }
+
+  serve() {
+    return (req, res, next) => {
+      next();
+    }    
+  }
+
+  delete(filename) {
+    return new Promise((resolve, reject) => {
+      this.client.deleteObject({
+        ...baseParams,
+        Key: this.basePath + filename
+      }, (err, data) => {
+        if(err) {
+          reject(this.errorParser(err))   
+        }else {
+          resolve(true)
+        }
+      })
+    })
+  }
+
+  read(filename) {
+    return new Promise((resolve, reject) => {
+      this.client.getObject({
+        ...baseParams,
+        Key: this.basePath + filename
+      }, (err, data) => {
+        if(err) {
+          reject(this.errorParser(err))        
+        }else {
+          resolve(data.Body ? data.Body.toString() : '')
+        }
+      })
+    })
+  }
+
+  errorParser(err) {
+    if(err.statusCode === 404) {
+      return 'Resource Not Exists'
+    }else if(err.statusCode === 403){
+      return 'AccessDenied'
+    }else {
+      return err
+    }
+  }
+
+}
+
+module.exports = CosStore
